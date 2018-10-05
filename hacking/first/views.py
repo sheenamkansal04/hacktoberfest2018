54c41e0fe9ef56756ae6ae38d9928ebf1b6411bc
Sheenam Kansal <sheenamkansal99@gmail.com>
2018-10-05 22:31:05 +0530

    FirstProgram

--- hacking/first/views.py
+++ hacking/first/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def first_hack(request):
+    return HttpResponse("<h1>This is first hacking program.</h1>")
