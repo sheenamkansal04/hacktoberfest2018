7ed8b075d3344486ac9caf826fdd2cfb42579784
Sheenam Kansal <sheenamkansal99@gmail.com>
2018-10-05 23:03:54 +0530

    Add Models

--- hacking/first/models.py
+++ hacking/first/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class UserModel(models.Model):
+
+    email = models.EmailField(max_length=255)
+    name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
