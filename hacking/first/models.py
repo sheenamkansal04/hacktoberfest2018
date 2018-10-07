10ec9fddbcb42f9dc5db655ab3dabc9d93fb5ec3
Sheenam Kansal <sheenamkansal99@gmail.com>
2018-10-07 13:02:23 +0530

    Finally

--- hacking/first/models.py
+++ hacking/first/models.py
@@ -6,4 +6,4 @@ class UserModel(models.Model):
     email = models.EmailField(max_length=255)
     name = models.CharField(max_length=255)
     username = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
+    
